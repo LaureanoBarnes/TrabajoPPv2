e9b442a7c00a345547f4e68f5818542e1ead7129
Laureano Barnes <laureanobarnes@hotmail.com>
2024-10-05 15:37:44 -0300

    Subir pdf, Visualizar, Eliminar y editar. V3.0.0

--- src/main/java/holaSpringData/servicio/UploadService.java
+++ src/main/java/holaSpringData/servicio/UploadService.java
@@ -14,14 +14,21 @@ import java.util.Objects;
 
 @Service
 public class UploadService {
+    private String sanitizeFileName(String fileName) {
+        return fileName.replaceAll("[^a-zA-Z0-9.]", "_");
+    }
 
     private final String url = "upload/";
 
     public String saveUpload(MultipartFile file) throws IOException {
         if (!file.isEmpty()){
             byte [] bytes = file.getBytes();
+            // Limpiar el nombre del archivo
+            String originalFileName = Objects.requireNonNull(file.getOriginalFilename());
+            String sanitizedFileName = sanitizeFileName(originalFileName);
             // Codificar el nombre del archivo
-            String encodedFileName = URLEncoder.encode(Objects.requireNonNull(file.getOriginalFilename()), StandardCharsets.UTF_8);
+            String encodedFileName = URLEncoder.encode(sanitizedFileName, StandardCharsets.UTF_8);
+
             Path path = Paths.get(url + encodedFileName);
             Files.write(path, bytes);
             return encodedFileName;
--- src/main/java/holaSpringData/web/AulaController.java
+++ src/main/java/holaSpringData/web/AulaController.java
@@ -1,22 +1,27 @@
 package holaSpringData.web;
 
+import holaSpringData.clases.Archivos;
 import holaSpringData.clases.Aula;
 import holaSpringData.clases.Individuo;
-import holaSpringData.servicio.AulaServicio;
-import holaSpringData.servicio.IndividuoServicio;
-import holaSpringData.servicio.UsuarioAutenticacionServicio;
+import holaSpringData.dao.ArchivoDao;
+import holaSpringData.servicio.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 public class AulaController {
+    @Autowired
+    private PdfService pdfService;
 
     @Autowired
     private AulaServicio aulaServicio;
@@ -83,8 +88,9 @@ public class AulaController {
     @PostMapping("/aula/{id_aula}/agregarUsuario")
     public String agregarUsuario(@PathVariable("id_aula") Long id_aula, @RequestParam("id_individuo") Long id_individuo) {
         aulaServicio.agregarUsuario(id_aula, id_individuo);
-        return "redirect:/aula/" + id_aula ;
+        return "redirect:/aula/" + id_aula;
     }
+
     @PostMapping("/{id_aula}/eliminarAdministrador")
     public String eliminarAdministrador(@PathVariable("id_aula") Long id_aula, @RequestParam("id_individuo") Long id_individuo) {
         aulaServicio.eliminarAdministrador(id_aula, id_individuo);
@@ -97,6 +103,38 @@ public class AulaController {
         return "redirect:/aulas/editar/" + id_aula;
     }
 
+    @PostMapping("/archivo/eliminar/{id}/{id_aula}")
+    public String eliminarArchivo(@PathVariable Long id, @PathVariable Long id_aula, RedirectAttributes redirectAttributes) {
+        try {
+            pdfService.delete(id);  // Implementa este método en tu servicio
+            redirectAttributes.addFlashAttribute("mensaje", "Archivo eliminado correctamente");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "No se pudo eliminar el archivo");
+        }
+        return "redirect:/aula/" + id_aula;  // Redirige a la lista de archivos o la página que desees
+    }
+
+    @GetMapping("/archivo/editar/{id}/{id_aula}")
+    public String editarArchivo(@PathVariable Long id, @PathVariable Long id_aula, Model model) {
+        Archivos archivo = pdfService.get(id);
+        model.addAttribute("archivo", archivo);
+        model.addAttribute("id_aula", id_aula); // Pasar el ID del aula para redirigir después
+        return "redirect:/aula/" + id_aula; // Devuelve la vista "aula" donde se pueden mostrar los archivos y el formulario
+    }
+
+
+    @PostMapping("/archivo/editar/{id}/{id_aula}")
+    public String actualizarArchivo(@PathVariable Long id, @PathVariable Long id_aula, @ModelAttribute Archivos archivo, @RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        try {
+            pdfService.update(id, archivo, file);
+            redirectAttributes.addFlashAttribute("mensaje", "Archivo actualizado correctamente");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "No se pudo actualizar el archivo");
+        }
+        return "redirect:/aula/" + id_aula; // Redirige a la página del aula
+    }
+
+
 
 
     @GetMapping("/aula/{id_aula}")
@@ -106,6 +144,10 @@ public class AulaController {
         if (aula == null) {
             return "redirect:/"; // Redirige a una página de error si el aula no existe
         }
+
+
+        List<Archivos> archivos = pdfService.getArchivos();
+        model.addAttribute("archivos", archivos);
 
 
         Individuo currentUser = individuoServicio.findByNomusuario(usuario.getUsername());
